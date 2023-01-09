39747fc8bfa9968164d4297669bf69056ff436f9
spwt01 <51329196+spwt01@users.noreply.github.com>
2023-01-09 12:39:45 +0800

    init upload

---
+++ public/firebase.js
@@ -0,0 +1,17 @@
+
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyAyESIl44ETEPSeLbJgebPYLQd_Nhx3qxQ",
+    authDomain: "personal-f3b2b.firebaseapp.com",
+    projectId: "personal-f3b2b",
+    storageBucket: "personal-f3b2b.appspot.com",
+    messagingSenderId: "1060374825828",
+    appId: "1:1060374825828:web:02d4de7724735388680871",
+    measurementId: "G-M6VHNNFDF6"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  
